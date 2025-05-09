a9bbfa88c3cdcc8cff51dee08df1c38652646edf
Syed-Ashiq-AP <syedashiq.ap@gmail.com>
2025-05-09 08:46:41 +0530

    session types

---
+++ types/next-auth.d.ts
@@ -0,0 +1,22 @@
+import NextAuth from "next-auth";
+
+declare module "next-auth" {
+  interface Session {
+    user: {
+      _id: string;
+      name?: string | null;
+      email?: string | null;
+      image?: string | null;
+    };
+  }
+
+  interface User {
+    _id: string;
+  }
+}
+
+declare module "next-auth/jwt" {
+  interface JWT {
+    _id: string;
+  }
+}
